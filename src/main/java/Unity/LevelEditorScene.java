4c3956371652652dddf6e9faa279b31ce13bdea9
D A GURUPRIYAN <am400718@gmail.com>
2023-03-24 19:57:44 +0530

    initial commit

---
+++ src/main/java/Unity/LevelEditorScene.java
@@ -0,0 +1,114 @@
+package Unity;
+
+
+import org.lwjgl.BufferUtils;
+import renderer.Shader;
+
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
+
+import static org.lwjgl.opengl.ARBVertexArrayObject.glBindVertexArray;
+import static org.lwjgl.opengl.ARBVertexArrayObject.glGenVertexArrays;
+import static org.lwjgl.opengl.GL20.*;
+
+public class LevelEditorScene extends Scene {
+    private String vertexShaderSrc = "#version 330 core\n" +
+            "layout (location=0) in vec3 aPos;\n" +
+            "layout (location=1) in vec4 aColor;\n" +
+            "\n" +
+            "out vec4 fColor;\n" +
+            "\n" +
+            "void main(){\n" +
+            "    fColor = aColor;\n" +
+            "    gl_Position = vec4(aPos, 1.0);\n" +
+            "}";
+    private String fragmentShaderSrc = "#version 330 core\n" +
+            "\n" +
+            "in vec4 fColor;\n" +
+            "\n" +
+            "out vec4 color;\n" +
+            "\n" +
+            "void main(){\n" +
+            "    color = fColor;\n" +
+            "}";
+    private int vertexID, fragmentID, shaderProgram;
+    private float[] vertexArray = {
+            //position              // color
+            0.5f,-0.5f,0.0f,        1.0f,0.0f,0.0f,1.0f,//bottom right
+            -0.5f,0.5f,0.0f,        0.0f,1.0f,0.0f,1.0f,// top left
+            0.5f,0.5f,0.0f,         0.0f,0.0f,1.0f,1.0f,//top right
+            -0.5f,-0.5f,0.0f,       1.0f,1.0f,0.0f,1.0f,//bottom left
+    };
+    // IMPORTANT: Must be counterclockwise order
+    private int [] elementArray = {
+            2,1,0,//Top right triangle
+            0,1,3//bottom left triangle
+    };
+
+    private int vaoID, vboID, eboID;
+    private  Shader defaultShader;
+
+    public LevelEditorScene() {
+
+
+    }
+
+    @Override
+    public void init(){
+        defaultShader = new Shader("assets/shaders/default.glsl");
+        defaultShader.compile();
+
+        // Generate VAO, VBO and EBO buffer objects and send to gpu
+        vaoID = glGenVertexArrays();
+        glBindVertexArray(vaoID);
+
+        // Create a float buffer of vertices
+        FloatBuffer vertexBuffer = BufferUtils.createFloatBuffer(vertexArray.length);
+        vertexBuffer.put(vertexArray).flip();
+
+        // Create VBO upload the vertex buffer
+        vboID = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vboID);
+        glBufferData(GL_ARRAY_BUFFER, vertexBuffer, GL_STATIC_DRAW);
+
+        // Create the indices and upload
+        IntBuffer elementBuffer = BufferUtils.createIntBuffer(elementArray.length);
+        elementBuffer.put(elementArray).flip();
+
+        eboID = glGenBuffers();
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, eboID);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, elementBuffer, GL_STATIC_DRAW);
+
+        // Add the vertex attribute pointers
+        int positionSize = 3;
+        int colorSize = 4;
+        int floatSizeBytes = 4;
+        int vertexSizeBytes = (positionSize + colorSize) * floatSizeBytes;
+        glVertexAttribPointer(0, positionSize, GL_FLOAT, false, vertexSizeBytes, 0);
+        glEnableVertexAttribArray(0);
+
+        glVertexAttribPointer(1, colorSize, GL_FLOAT, false, vertexSizeBytes, positionSize * floatSizeBytes);
+        glEnableVertexAttribArray(1);
+    }
+    @Override
+    public void update(float dt) {
+        defaultShader.use();
+        // Bind the vao we're using
+        glBindVertexArray(vaoID);
+
+        // Enable the vertex attribute pointers
+        glEnableVertexAttribArray(0);
+        glEnableVertexAttribArray(1);
+
+        glDrawElements(GL_TRIANGLES, elementArray.length, GL_UNSIGNED_INT, 0);
+
+        // Unbind everything
+        glDisableVertexAttribArray(0);
+        glDisableVertexAttribArray(1);
+
+        glBindVertexArray(0);
+
+        defaultShader.detach();
+    }
+
+}
---
+++ src/main/java/Unity/Scene.java
@@ -0,0 +1,12 @@
+package Unity;
+
+public abstract class Scene {
+    public Scene(){
+
+    }
+    public void init(){
+
+    }
+
+    public abstract void update(float dt);
+}
