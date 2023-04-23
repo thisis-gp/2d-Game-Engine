f1ea794bb52af6ed08848501cc098ec0dfe93f6a
D A GURUPRIYAN <am400718@gmail.com>
2023-04-23 12:56:11 +0530

    Z-Indexing

--- src/main/java/Unity/GameObject.java
+++ src/main/java/Unity/GameObject.java
@@ -8,17 +8,21 @@ public class GameObject {
     private String name;
     private List<Component> components;
     public Transform transform;
+    private int zIndex;
 
     public GameObject(String name) {
         this.name = name;
+        this.zIndex = 0;
         this.components = new ArrayList<>();
         this.transform = new Transform();
     }
 
-    public GameObject(String name, Transform transform) {
+    public GameObject(String name, Transform transform, int zIndex) {
         this.name = name;
+        this.zIndex = zIndex;
         this.components = new ArrayList<>();
         this.transform = transform;
+
     }
 
     public <T extends Component> T getComponent(Class<T> componentClass) {
@@ -61,5 +65,9 @@ public class GameObject {
         for (int i=0; i < components.size(); i++) {
             components.get(i).start();
         }
+    }
+
+    public int zIndex(){
+        return this.zIndex;
     }
 }
--- src/main/java/Unity/LevelEditorScene.java
+++ src/main/java/Unity/LevelEditorScene.java
@@ -25,13 +25,20 @@ public class LevelEditorScene extends Scene {
 
         sprites = AssetPool.getSpritesheet("assets/Images/spritesheet.png");
 
-        obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
-        obj1.addComponent(new SpriteRenderer(sprites.getSprite(0)));
+
+        obj1 = new GameObject("Object 1", new Transform(new Vector2f(200, 100), new Vector2f(256, 256)),2);
+        obj1.addComponent(new SpriteRenderer(new Sprite(
+                AssetPool.getTexture("assets/Images/blendImage1.png")
+        )));
         this.addGameObjectToScene(obj1);
 
-        GameObject obj2 = new GameObject("Object 2", new Transform(new Vector2f(400, 100), new Vector2f(256, 256)));
-        obj2.addComponent(new SpriteRenderer(sprites.getSprite(10)));
+        GameObject obj2 = new GameObject("Object 2", new Transform(new Vector2f(400, 100), new Vector2f(256, 256)),1);
+        obj2.addComponent(new SpriteRenderer(new Sprite(
+                AssetPool.getTexture("assets/Images/blendImage2.png")
+        )));
         this.addGameObjectToScene(obj2);
+
+
 
     }
 
@@ -40,22 +47,11 @@ public class LevelEditorScene extends Scene {
         AssetPool.addSpritesheet("assets/Images/spritesheet.png", new Spritesheet(AssetPool.getTexture("assets/images/spritesheet.png"),16,16,26,0));
     }
 
-    private  int spriteIndex = 0;
-    private float spriteFlipTime = 0.2f;
-    private float spriteFlipTimeLeft = 0.0f;
+
 
     @Override
     public void update(float dt) {
-        spriteFlipTimeLeft -= dt;
 
-        if (spriteFlipTimeLeft <= 0){
-            spriteFlipTimeLeft = spriteFlipTime ;
-            spriteIndex++;
-            if (spriteIndex > 4){
-                spriteIndex = 0;
-            }
-            obj1.getComponent(SpriteRenderer.class).setSprite(sprites.getSprite(spriteIndex));
-        }
 
         for (GameObject go : this.gameObjects) {
             go.update(dt);
