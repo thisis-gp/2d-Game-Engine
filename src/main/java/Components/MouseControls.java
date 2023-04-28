238941c220f5da338694a5104dd6527fb3a5b56a
D A GURUPRIYAN <am400718@gmail.com>
2023-04-28 22:45:47 +0530

    Grid

--- src/main/java/Components/MouseControls.java
+++ src/main/java/Components/MouseControls.java
@@ -1,9 +1,9 @@
 package Components;
 
-
 import Unity.GameObject;
 import Unity.MouseListener;
 import Unity.Window;
+import Util.Settings;
 
 import static org.lwjgl.glfw.GLFW.GLFW_MOUSE_BUTTON_LEFT;
 
@@ -22,8 +22,10 @@ public class MouseControls extends Component {
     @Override
     public void update(float dt) {
         if (holdingObject != null) {
-            holdingObject.transform.position.x = MouseListener.getOrthoX() - 16;
-            holdingObject.transform.position.y = MouseListener.getOrthoY() - 16;
+            holdingObject.transform.position.x = MouseListener.getOrthoX();
+            holdingObject.transform.position.y = MouseListener.getOrthoY();
+            holdingObject.transform.position.x = (int)(holdingObject.transform.position.x / Settings.GRID_WIDTH) * Settings.GRID_WIDTH;
+            holdingObject.transform.position.y = (int)(holdingObject.transform.position.y / Settings.GRID_HEIGHT) * Settings.GRID_HEIGHT;
 
             if (MouseListener.mouseButtonDown(GLFW_MOUSE_BUTTON_LEFT)) {
                 place();
