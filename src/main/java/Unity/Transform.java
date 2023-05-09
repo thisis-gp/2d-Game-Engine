9f440ce09988a0805c72f672241a78a035a3f18d
D A GURUPRIYAN <am400718@gmail.com>
2023-05-09 18:06:53 +0530

    Gizmos

--- src/main/java/Unity/Transform.java
+++ src/main/java/Unity/Transform.java
@@ -6,6 +6,7 @@ public class Transform {
 
     public Vector2f position;
     public Vector2f scale;
+    public float rotation = 0.0f;
 
     public Transform() {
         init(new Vector2f(), new Vector2f());
@@ -24,24 +25,21 @@ public class Transform {
         this.scale = scale;
     }
 
-    public Transform copy(){
+    public Transform copy() {
         return new Transform(new Vector2f(this.position), new Vector2f(this.scale));
     }
 
-    public void copy(Transform to){
+    public void copy(Transform to) {
         to.position.set(this.position);
         to.scale.set(this.scale);
     }
 
     @Override
     public boolean equals(Object o) {
-        if (o == null) {
-            return false;
-        }
-        if (o instanceof Transform) {
-            return false;
-        }
-        Transform t = (Transform) o;
+        if (o == null) return false;
+        if (!(o instanceof Transform)) return false;
+
+        Transform t = (Transform)o;
         return t.position.equals(this.position) && t.scale.equals(this.scale);
     }
 }
